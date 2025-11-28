3aa83de82772de2ec73b4e31eb5aa8e4b98efcda
PyroScorcher <pyrobackupacc@gmail.com>
2025-11-28 20:50:43 +0700

    typo lagi

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials"; // <--- Import it
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
