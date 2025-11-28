30ab8b65a29ddca0a56d8a5ee3b8a4075e6bc701
PyroScorcher <pyrobackupacc@gmail.com>
2025-11-28 21:04:24 +0700

    fix color

--- components/Testi.tsx
+++ components/Testi.tsx
@@ -11,7 +11,7 @@ const testimonialImages = [
 
 export default function Testimonials() {
   return (
-    <section className="py-16 bg-zinc-50 dark:bg-black overflow-hidden">
+    <section className="py-16 bg-[#DDA66D] overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Title */}
